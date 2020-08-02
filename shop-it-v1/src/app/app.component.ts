b4d6a40eddb3cf16491df36473539a1aaba550b8
Nagabhushanam <nagabhushanamn@gmail.com>
2020-08-02 16:33:14 +0530

    NG intro

---
+++ shop-it-v1/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+
+  items = [
+    {
+      id: 1,
+      name: 'Laptop',
+      price: 149000,
+      description: 'New mac pro',
+      canBuy: true,
+      imagePath: "assets/Laptop.png"
+    },
+    {
+      id: 2,
+      name: 'Mobile',
+      price: 1000,
+      description: 'New  pro',
+      canBuy: true,
+      imagePath: "assets/Mobile.png"
+    },
+
+  ]
+
+}
